912e2387ac7efb45f66eeb6e1d16edb0264da227
Nishita Rani <92663360+Nishita0807@users.noreply.github.com>
2023-09-09 14:01:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+ if (str.length === 0) {
+    return null; // Return null for empty input
+  }
+
+  const charCount = {}; // Object to store character frequencies
+
+  // Count the frequency of each character in the input
+  for (const char of str) {
+    if (charCount[char]) {
+      charCount[char]++;
+    } else {
+      charCount[char] = 1;
+    }
+  }
+
+  // Find the first non-repeated character
+  for (const char of str) {
+    if (charCount[char] === 1) {
+      return char; // Return the first non-repeated character
+    }
+	  
+  }
+
+	
+  return null; // Return null if all characters are repeated
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
